2841fa9ca3b3d041012be1d45c8841312b213feb
Paul Oskar Soe <35954675+MaikPython@users.noreply.github.com>
2021-11-01 17:53:24 +0200

    JSI camera setup

--- app/JSI/JSICamera.tsx
+++ app/JSI/JSICamera.tsx
@@ -1,15 +1,39 @@
-import React, {FC, useRef} from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {FC, useRef, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Camera} from 'react-native-vision-camera';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import Reanimated from 'react-native-reanimated';
 
 interface IJSICamera {}
 
+const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera);
+Reanimated.addWhitelistedNativeProps({
+  zoom: true,
+});
+
 const JSICamera: FC<IJSICamera> = () => {
+  const [cameraPosition, setCameraPosition] = useState<'front' | 'back'>(
+    'back',
+  );
+
   const camera = useRef<Camera>(null);
+  const devices = useCameraDevices();
+  const device = devices[cameraPosition];
+
+  console.log('device:', device);
 
   return (
     <View style={styles.container}>
-      <Text>JSI camera</Text>
+      {device && (
+        <ReanimatedCamera
+          style={StyleSheet.absoluteFill}
+          isActive
+          ref={camera}
+          device={device}
+          photo
+          video
+        />
+      )}
     </View>
   );
 };
@@ -19,5 +43,6 @@ export default JSICamera;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black',
   },
 });
