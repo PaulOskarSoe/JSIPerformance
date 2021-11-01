861378cdfb0b8ac788647e6adf7668e139ebca1e
Paul Oskar Soe <35954675+MaikPython@users.noreply.github.com>
2021-11-01 19:07:12 +0200

    navigation added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import JSICamera from './app/JSI/JSICamera';
+import JSICamera from './app/jsi/JSICamera';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import BridgeCamera from './app/bridge/BridgeCamera';
+import SettingsScreen from './app/settings/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return <JSICamera />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Screen name="JSI" component={JSICamera} />
+        <Tab.Screen name="Bridge" component={BridgeCamera} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
